97e549d6bc8c8c2c719329e4e15561201201c96d
Keith <kayceecode@gmail.com>
2024-11-13 08:53:45 -0500

    Started Login and Map. Added CSS for background

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+import Head from 'next/head';
+import store from '@/redux/store';
+
+export default function page() {
+  return (
+    <div className=''>
+      <div className=''>
+
+      </div>
+    </div>
+  );
+}
--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,32 +1,49 @@
-'use server';
+'use client';
 
 import { addOne } from '@/redux/slices/counterSlice';
 import store from '@/redux/store';
 import Head from 'next/head';
-import React from 'react';
-import Map from 'react-map-gl';
+import React, { useEffect, useState } from 'react';
+import Map, { Marker } from 'react-map-gl';
 
-export async function getServerSideProps() {
-  const res = await fetch('https://localhost:');
-  const data = await res.json();
+// export async function getServerSideProps() {
+//   const res = await fetch('https://localhost:');
+//   const data = await res.json();
 
 
-  return {
-    props: {
-      properties: data.items,
-      // properties: JSON.parse(JSON.stringify(data.properties))
-    },
-  }
-}
+//   return {
+//     props: {
+//       properties: data.items,
+//       // properties: JSON.parse(JSON.stringify(data.properties))
+//     },
+//   }
+// }
+
+// const GetBusinesses = async () => {
+//   let response = await fetch("https://localhost:5155/api/businesses/location/Baltimore, MD");
+//   let data = response.json();
+
+//   return data;
+// }
 
 export default function MapPage() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    fetch('http://localhost:5155/api/businesses/location/NewYork')
+    .then(response => response.json())
+    .then(data => setData(data))
+    .then(data => console.log("Data: ", data))
+    .catch(error => console.error('Error fetching data:', error));
+  }, []);
+  
   return (
           <div>
             <Head>
-              <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v<YOUR_MAPBOX_VERSION>/mapbox-gl.css' rel='stylesheet' />
+              <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v3.7.0/mapbox-gl.css' rel='stylesheet' />
               {/* Add additional global head tags here */}
             </Head>
-            <Map
+            {/* <Map
               mapboxAccessToken={process.env.NEXT_PUBLIC_MapboxAccessTokenDev}
               initialViewState={{
                 longitude: -122.4,
@@ -35,7 +52,14 @@ export default function MapPage() {
               }}
               style={{width: "100%", height: "70vh"}}
               mapStyle="mapbox://styles/mapbox/streets-v9"
-              />
+              > */}
+                {/* {data != null ? data.map(() => 
+
+                ) : null} */}
+                {/* <Marker longitude={-100} latitude={40} anchor="bottom" >
+                  <img src="./images/black_map_marker.png" />
+                </Marker>   */}
+              {/* </Map> */}
           </div>
   );
 }
