063ece4be6c3df0ef7505bfcdc53b53c46c7deb2
Keith <kayceecode@gmail.com>
2025-11-08 21:24:58 -0500

    removed primereact. edited login/form

--- notes.md
+++ notes.md
@@ -16,9 +16,31 @@
     A component that needs to be declared a client component is Redux's store provider or another provider that uses state management such as context api. Commonly in other HOCs. These are client side tools. 
 
 **September 12th**
-In React, functions are re-created on every render. If you pass an inline function to useEffect and put it in the dependency array, the effect will re-run on every render because the function reference changes each time. To avoid that, you can wrap the function in useCallback, which memoizes the reference and only re-creates it when its dependencies change. This makes it safe to include in the dependency array without causing unnecessary re-runs. However, if you omit dependencies, you risk stale closures where your callback captures outdated state or props.
+1. In React, functions are re-created on every render. If you pass an inline function to useEffect and put it in the dependency array, the effect will re-run on every render because the function reference changes each time. To avoid that, you can wrap the function in useCallback, which memoizes the reference and only re-creates it when its dependencies change. This makes it safe to include in the dependency array without causing unnecessary re-runs. However, if you omit dependencies, you risk stale closures where your callback captures outdated state or props.
+
 **October 27th**
-Tailwind/Flexbox has flex-grow-0 aka "grow-0" & flex-0. Flex-0 sets flexbasis to 0 cause it's a short hand. You should use grow-0 over flex-0. Grow-0 on an item will leave flex-basis & flexshrink to their defaults (flexbasis 0%, flexshrink 1) so there's no need for w-fit or w-auto on an item's class. 
+1. Tailwind/Flexbox has flex-grow-0 aka "grow-0" & flex-0. Flex-0 sets flexbasis to 0 cause it's a short hand. You should use grow-0 over flex-0. Grow-0 on an item will leave flex-basis & flexshrink to their defaults (flexbasis 0%, flexshrink 1) so there's no need for w-fit or w-auto on an item's class. 
+
 **Oct 28**
-SetErrors are field specific.
-SetStatus is general use of the form.
+1. SetErrors are field specific.
+2. SetStatus is general use of the form.
+
+**Nov 5th**
+1. Formiks prop types will take in types in this order Values, Status = any, when assigning types to Formik.
+    example:
+```ts
+    interface FormikProps<Values, Status = any> {
+        values: Values;
+        status?: Status;
+        errors: FormikErrors<Values>;
+        touched: FormikTouched<Values>;
+        isSubmitting: boolean;
+    }
+    <Formik<MyValues, MyFormStatus>
+        intialValues={initialValues}>
+        {({ status }) => (
+            <>
+                {status?.errors && <p>{status.errors}</p>}
+            </>
+        )}
+    </Formik>
--- src/app/login/form.tsx
+++ src/app/login/form.tsx
@@ -1,85 +1,168 @@
-'use client'
+"use client";
 
-import React, { useState } from 'react';
-import { Formik, Form, Field, ErrorMessage, FormikHelpers } from 'formik';
-import { useRouter } from 'next/navigation';
-import * as Yup from 'yup';
-import Loader from '../components/ui/loader/Loader';
-import { AuthError, AuthSuccessResponse, LoginRequest, LoginResponse } from '../../../types/auth';
-import { ApiResult } from '../../../types/api';
-import { useAuth } from '../../hooks/useAuth';
-import Link from 'next/link';
+import React, { useState } from "react";
+import {
+  Formik,
+  Form,
+  Field,
+  ErrorMessage,
+  FormikHelpers,
+  FormikState,
+  FormikProps,
+} from "formik";
+import { useRouter } from "next/navigation";
+import * as Yup from "yup";
+import Loader from "../components/ui/loader/Loader";
+import {
+  AuthError,
+  AuthSuccessResponse,
+  LoginRequest,
+  LoginResponse,
+} from "../../../types/auth";
+import { ApiResult } from "../../../types/api";
+import { useAuth } from "../../hooks/useAuth";
+import Link from "next/link";
+import { AnimatePresence, motion } from "framer-motion";
+import { Button } from "primereact/button";
+import { Password } from "primereact/password";
+import { classNames } from "primereact/utils";
+
+interface FormValues {
+  email: string;
+  password: string;
+}
+
+interface FormStatus {
+  error?: string;
+}
 
 export default function LoginForm() {
-    const router = useRouter();
-    const { login } = useAuth();
+  const router = useRouter();
+  const { login } = useAuth();
 
-    const handleSubmit = async (values: LoginRequest, formikHelpers: FormikHelpers<LoginRequest>) => {
-        const { setSubmitting, setStatus } = formikHelpers;
-        setSubmitting(true);
-        let response: AuthSuccessResponse | undefined = undefined;
+  const handleSubmit = async (
+    values: LoginRequest,
+    formikHelpers: FormikHelpers<LoginRequest>
+  ) => {
+    const { setSubmitting, setStatus } = formikHelpers;
+    setSubmitting(true);
+    let response: AuthSuccessResponse | undefined = undefined;
 
-        try {
-            response = await login(values);
-            if (response.success) {
-                //onSuccess?.();
-                // alert(JSON.stringify(values, null, 2));
-                //await new Promise((resolve) => setTimeout(resolve, 1000));
-                router.push('/');
-                return;
-            }
-            setStatus({error: response.message || 'Invalid credentials!'})
-        } catch(error: unknown) {
-            const message = error instanceof Error ? error.message : 'An unexpected error occurred';
-            setStatus({ error: message });
-        } finally {
-            if (!response?.success) // prevent state update after navigation
-                setSubmitting(false);
-        }
+    try {
+      response = await login(values);
+      if (response.success) {
+        //onSuccess?.();
+        // alert(JSON.stringify(values, null, 2));
+        //await new Promise((resolve) => setTimeout(resolve, 1000));
+        router.push("/");
+        return;
+      }
+      setStatus({ error: response.message || "Invalid credentials!" });
+    } catch (error: unknown) {
+      const message =
+        error instanceof Error ? error.message : "An unexpected error occurred";
+      setStatus({ error: message });
+    } finally {
+      if (!response?.success)
+        // prevent state update after navigation
+        setSubmitting(false);
     }
- 
-    const schema = Yup.object({
-        email: Yup.string().email('Invalid email').required('Email is required'),
-        password: Yup.string().required('Password is required'),
-    });
+  };
 
-    return (    
-        <div>  
-            <Formik
-                initialValues={{ email: 'tHiddle@gmail.com', password: 'somepw' }}// loginCredentials would be used here
-                validationSchema={schema}
-                onSubmit={handleSubmit}
-            >
-            {({ isSubmitting, status }) => (
+  const schema = Yup.object({
+    email: Yup.string().email("Invalid email").required("Email is required"),
+    password: Yup.string().required("Password is required"),
+  });
 
-                isSubmitting ? 
-                <Loader /> 
-                :
-                <div className='mx-6 px-8 py-12 rounded-lg border-stone-700 bg-neutral-800 border-[0.9px] w-96 container'>
-                    <h1 className="text-2xl font-bold mb-6 text-center text-neutral-300">
-                        Login to Foodies
-                    </h1>
-                    <Form className="flex justify-center flex-col">
-                        {status?.error && <div className="invisible opacity-0 transition-opacity duration-750">{status.error}</div>}
-                        <div className='mb-6 text-neutral-300'>
-                            <label htmlFor="email" className="w-full text-sm">Email</label>
-                            <Field type="email" name="email" disabled={isSubmitting} className="w-full px-3 py-2 text-gray-300 border bg-neutral-700 border-stone-600 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
-                            <ErrorMessage className="text-red-600 text-sm absolute" name="email" component="div" />
-                        </div>
-                        <div className='mb-6 text-neutral-300'>
-                            <label htmlFor="password" className="w-full text-sm">Password</label>
-                            <Field type="password" name="password" disabled={isSubmitting} className="mb-4 w-full px-3 py-2 text-gray-300 border bg-neutral-700 border-stone-600 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
-                            <ErrorMessage name="password" component="div" className="text-red-800" />
-                        </div>
-                        <button type="submit" className="border-stone-500 rounded border-[0.9px] py-2" disabled={isSubmitting}>
-                            Submit
-                        </button>
-                    </Form>
-                    <div className="text-center text-xs mt-4">
-                        Don't have an account <Link className="" href="/signup"><u>sign up</u></Link> today. 
-                    </div>
-                </div>
-            )}
-            </Formik>
-        </div>
-)};
+  // const isFormFieldInvalid = (formik: FormikProps<FormValues>, fieldName: string) => {
+  //     return !!(formik.touched[fieldName as keyof FormValues] && formik.errors[fieldName as keyof FormValues]);
+  // }
+
+  const isFormFieldInvalid = (
+    formik: FormikProps<FormStatus>,
+    fieldName: string
+  ) => {
+    return;
+  };
+
+  return (
+    <AnimatePresence>
+      <Formik
+        initialValues={{ email: "Hiddle@gmail.com", password: "somepw" }} // loginCredentials would be used here
+        validationSchema={schema}
+        onSubmit={handleSubmit}
+      >
+        {(formik) => (
+          <div className="mx-6 px-8 py-12 rounded-lg border-stone-700 bg-neutral-800 border-[0.9px] w-96 container">
+            <h1 className="text-2xl font-bold mb-6 text-center text-neutral-300">
+              Login to Foodies
+            </h1>
+            <Form className="flex justify-center flex-col">
+              {formik.status?.error && (
+                <motion.div
+                  key="status-errors"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.4, ease: "easeInOut" }}
+                >
+                  <div className="text-red-500 text-xs text-center border border-0.9 border-stone-700 rounded-sm max-w-1/3 mx-auto px-2 py-0.5">
+                    {formik.status?.error}
+                  </div>
+                </motion.div>
+              )}
+              {/* <div className={`transition-opacity duration-500 ease-in-out ${status?.errors ? "opacity-0" : "opacity-100"}`}>
+                            {status?.error ? status.error : ""}
+                        </div> */}
+              <div className="mb-6 text-neutral-300">
+                <label htmlFor="email" className="w-full text-sm">
+                  Email
+                </label>
+                <Field
+                  type="email"
+                  name="email"
+                  disabled={formik.isSubmitting}
+                  className="w-full px-3 py-2 text-gray-300 border bg-neutral-700 border-stone-600 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+                <ErrorMessage
+                  className="text-red-600 text-sm absolute"
+                  name="email"
+                  component="div"
+                />
+              </div>
+              <div className="mb-6 text-neutral-300">
+                <label htmlFor="password" className="w-full text-sm">
+                  Password
+                </label>
+                <Field
+                  type="password"
+                  name="password"
+                  disabled={formik.isSubmitting}
+                  className="mb-4 w-full px-3 py-2 text-gray-300 border bg-neutral-700 border-stone-600 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+                <ErrorMessage
+                  name="password"
+                  component="div"
+                  className="text-red-800"
+                />
+              </div>
+              <Button
+                type="submit"
+                label="Submit"
+                className=" border-stone-500 rounded border-[0.9px] py-2"
+                disabled={formik.isSubmitting}
+              />
+            </Form>
+            <div className="text-center text-xs mt-4">
+              Don't have an account{" "}
+              <Link className="" href="/signup">
+                <u>sign up</u>
+              </Link>{" "}
+              today.
+            </div>
+          </div>
+        )}
+      </Formik>
+    </AnimatePresence>
+  );
+}
